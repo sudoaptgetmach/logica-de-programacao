e0f3a9e567f719b09cc987fe8ae4116cb422804e
Luiz <qluizmachado@gmail.com>
2024-10-21 15:24:50 -0300

    NumeroChecker (verifica se o número é positivo, negativo, par ou impar) adicionado.

---
+++ src/main/java/org/mach/exercises/NumeroChecker.java
@@ -0,0 +1,29 @@
+package org.mach.exercises;
+
+import java.util.Scanner;
+
+public class NumeroChecker {
+
+    public static void main(String[] args) {
+
+        int a;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira um valor inteiro aleatório:");
+        a = scanner.nextInt();
+
+        String comparador = a % 2 == 0 ? "O número " + a + " é par." : "O número " + a + " é impar.";
+
+        // A melhor maneira de fazer o comparador acima seria:
+        // String comparador = (a % 2 == 0) ? "O número " + a + " é par." : "O número " + a + " é impar.";
+
+        String comparadorTipo = (a > 0) ? "O número " + a + " é positivo." :
+                                (a < 0) ? "O número " + a + " é negativo." : "O número é zero";
+
+        System.out.println(comparador);
+        System.out.println(comparadorTipo);
+
+        scanner.close();
+
+    }
+}
